3c2f78243b9a4f74e96329c349cc549462090bd6
jcbrizu <juan.jcb@gmail.com>
2019-07-27 16:12:32 -0300

    Repo update

--- Workshop_Final_Profe/src/js/controllers/peopleController.js
+++ Workshop_Final_Profe/src/js/controllers/peopleController.js
@@ -37,43 +37,6 @@ function handleData(data) {
   $('#seeMore').attr("disabled", false)
 }
 
-<<<<<<< HEAD
-var dictionary = {
-  male: 'Masculino',
-  female: 'Femenino',
-  blue: 'azul',
-  red: 'rojo',
-  yellow: 'amarillo',
-  brown: 'marron',
-  'blue-gray': 'azul grisaseo',
-  hazel: 'avellana',
-  unknown: 'desconocido',
-  black: 'negro',
-  dark: 'oscuro',
-  white: 'blanco',
-  'green, yellow': 'verde, amarillo',
-  orange: 'naranja',
-  pink: 'rosa',
-  'red, blue': 'rojo, azul',
-  gold: 'dorado',
-  'n/a': 'N/D',
-  hermaphrodite: 'hermafrodita',
-  none: 'N/D'
-
-}
-
-
-function translateToSpanish(wordInEnglish) {
-  var word = wordInEnglish.toLowerCase()
-  var traduccion = dictionary[word]
-  if (traduccion) {
-    return traduccion
-  }
-  return word
-}
-
-=======
->>>>>>> c150bc4177836d7b78188925d0001517e6b49601
 function renderPerson(anclaSelector, person, id) {
   $(anclaSelector).append(`
       <tr id="person-${id}">
